299dfba98bb728cac1b8c946da3b0d035bd43924
Filip Gregor <filip.gregor98@gmail.com>
2023-10-08 15:23:20 +0200

    feat: add support for long flashcards.
    Flashcards that have multiple lines as question text are now supported.
    Empty flashcards print a warning and are not saved.

--- src-tauri/src/parsers/markdown.rs
+++ src-tauri/src/parsers/markdown.rs
@@ -1,7 +1,45 @@
-use tokio::io::{AsyncBufRead, AsyncBufReadExt};
+use std::io::Result;
+use tokio::io::{AsyncBufRead, AsyncBufReadExt, Lines};
 
 use crate::models::flashcard::Flashcard;
-use std::io::Result;
+
+pub async fn read_until_hr<R: AsyncBufRead + Unpin>(lines: &mut Lines<R>) -> Result<String> {
+    let mut text = String::new();
+    while let Some(line) = lines.next_line().await? {
+        // TODO: Something more generic for MD lines.
+        if line.starts_with("---") || line.starts_with("- - -") {
+            return Ok(text);
+        }
+        text.push_str(line.trim());
+        text.push('\n');
+    }
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        "A card was not terminated",
+    ))
+}
+
+/**
+ * Parse a long question, expecting a following format:
+ * #flashcard [#tags ...] (This line should already be read and tags passed!)
+ * question text (possibly over multiple lines)
+ * - - - (A line in markdown)
+ * answer text (possibly over multiple lines)
+ * - - - (A line in markdown)
+ */
+pub async fn long_question<R: AsyncBufRead + Unpin>(
+    reader: &mut Lines<R>,
+    _tags: Vec<String>,
+) -> Result<Flashcard> {
+    let question = read_until_hr(reader).await?;
+    let answer = read_until_hr(reader).await?;
+
+    Ok(Flashcard {
+        id: None,
+        question,
+        answer,
+    })
+}
 
 pub async fn read_markdown<R: AsyncBufRead + Unpin>(reader: R) -> Result<Vec<Flashcard>> {
     let mut flashcards = vec![];
@@ -16,27 +54,54 @@ pub async fn read_markdown<R: AsyncBufRead + Unpin>(reader: R) -> Result<Vec<Fla
         // ```
         // question text ... [#tags ...] #flashcard [#tags ...]
         // answer text (possibly over multiple lines)
-        // ---
+        // - - - (A line in markdown)
+        // Or the format
+        // #flashcard [#tags ...]
+        // question text (possibly over multiple lines)
+        // - - - (A line in markdown)
+        // answer text (possibly over multiple lines)
+        // - - - (A line in markdown)
         // ```
-        let mut it = line.split('#');
-        let question = it.next().unwrap().trim().to_string();
-        let tags: Vec<String> = it.map(|s| s.to_string()).collect();
-        if !tags.into_iter().any(|s| s.starts_with("flashcard")) {
-            continue;
-        }
-
-        let mut answer = String::new();
-        while let Some(line) = line_it.next_line().await? {
-            // TODO: Something more generic for MD lines.
-            if line.starts_with("---") || line.starts_with("- - -") {
-                flashcards.push(Flashcard {
-                    id: None,
-                    question,
-                    answer,
-                });
-                break;
+        if line.trim().starts_with("#flashcard") {
+            let it = line.split('#');
+            let tags: Vec<String> = it.map(|s| s.to_string()).collect();
+            let card = long_question(&mut line_it, tags).await?;
+            if card.question.is_empty() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Card cannot have empty question or answer text",
+                ));
             }
-            answer.push_str(line.as_str());
+            flashcards.push(card);
+        } else {
+            let mut it = line.split('#');
+            let question = it.next().unwrap().trim().to_string();
+            let tags: Vec<String> = it.map(|s| s.to_string()).collect();
+            // Only after this we are certain that this is a flashcard
+            if !tags.into_iter().any(|s| s.starts_with("flashcard")) {
+                continue;
+            }
+
+            if question.is_empty() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Card cannot have empty question text",
+                ));
+            }
+
+            let answer = read_until_hr(&mut line_it).await?;
+            if answer.is_empty() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Card cannot have empty answer text",
+                ));
+            }
+            println!("Question 2: >{}<", question);
+            flashcards.push(Flashcard {
+                id: None,
+                question,
+                answer,
+            });
         }
     }
     Ok(flashcards)
@@ -62,7 +127,7 @@ mod tests {
         let flashcards = read_markdown(reader).await.unwrap();
         assert_eq!(flashcards.len(), 1);
         assert_eq!(flashcards[0].question, "question");
-        assert_eq!(flashcards[0].answer, "answer");
+        assert_eq!(flashcards[0].answer, "answer\n");
     }
 
     #[tokio::test]
@@ -72,9 +137,9 @@ mod tests {
         let flashcards = read_markdown(reader).await.unwrap();
         assert_eq!(flashcards.len(), 2);
         assert_eq!(flashcards[0].question, "question");
-        assert_eq!(flashcards[0].answer, "answer");
+        assert_eq!(flashcards[0].answer, "answer\n");
         assert_eq!(flashcards[1].question, "question2");
-        assert_eq!(flashcards[1].answer, "answer2");
+        assert_eq!(flashcards[1].answer, "answer2\n");
     }
 
     #[tokio::test]
@@ -84,11 +149,11 @@ mod tests {
         let flashcards = read_markdown(reader).await.unwrap();
         assert_eq!(flashcards.len(), 3);
         assert_eq!(flashcards[0].question, "question");
-        assert_eq!(flashcards[0].answer, "answer");
+        assert_eq!(flashcards[0].answer, "answer\n");
         assert_eq!(flashcards[1].question, "question2");
-        assert_eq!(flashcards[1].answer, "answer2");
+        assert_eq!(flashcards[1].answer, "answer2\n");
         assert_eq!(flashcards[2].question, "question3");
-        assert_eq!(flashcards[2].answer, "answer3");
+        assert_eq!(flashcards[2].answer, "answer3\n");
     }
 
     #[tokio::test]
@@ -103,7 +168,44 @@ mod tests {
     async fn test_unterminated_question() {
         let markdown = "question #flashcard\nanswer\n";
         let reader = Cursor::new(markdown);
+        let flashcards = read_markdown(reader).await;
+        assert!(flashcards.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_long_question() {
+        let markdown = r#"
+Some text
+#flashcard
+a text
+of a question
+$x = x + 1$
+- - -
+An answer
+to the question
+- - -
+Some more text
+a normal card #flashcard
+a normal answer
+- - -
+"#;
+        let reader = Cursor::new(markdown);
         let flashcards = read_markdown(reader).await.unwrap();
-        assert_eq!(flashcards.len(), 0);
+        assert_eq!(flashcards.len(), 2);
+        assert_eq!(
+            flashcards[0].question,
+            "a text\nof a question\n$x = x + 1$\n"
+        );
+        assert_eq!(flashcards[0].answer, "An answer\nto the question\n");
+        assert_eq!(flashcards[1].question, "a normal card");
+        assert_eq!(flashcards[1].answer, "a normal answer\n");
+    }
+
+    #[tokio::test]
+    async fn test_empty_question() {
+        let markdown = "#flashcard\n---\n---\n";
+        let reader = Cursor::new(markdown);
+        let flashcards = read_markdown(reader).await;
+        assert!(flashcards.is_err());
     }
 }
